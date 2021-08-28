ffa155aeeb46cc129938d52fe0aa2226fe2e96b8
Hadley <hadley_dalton@brown.edu>
2021-08-28 19:18:16 -0400

    prog

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,26 @@
 function Header() {
     return (
         <div>
-            <img class="title-bg" src="https://images.fineartamerica.com/images-medium-large/pastel-sky-john-bartosik.jpg"></img>
-            <div class="circle"></div>
-            <div class="header-style">.hadleyDalton</div>
+            <img class="title-bg" src="https://i.pinimg.com/originals/15/71/05/15710504898649f93190a5626de1a74c.jpg"></img>
+            <div class="grad-circle"></div>
+            <div class="header-style">.hadleyDalton()</div>
             <div class="subtext-style">Designer and coder from {<b>Brown University</b>}</div>
+            <div class="cr-style">© Hadley Dalton 2021</div>
+            <div class="cr-line"></div>
+            <div class="solid-border-circle"></div>
+            <div class="filled-circle"></div>
+            <div class="dotted-circle"></div>
+            <div class="title-bg-border"></div>
         </div>
     )
 }
 
 export default Header
+
+/* 
+https://wallpapercave.com/wp/wp5134857.png
+(*) https://i.pinimg.com/originals/15/71/05/15710504898649f93190a5626de1a74c.jpg
+https://wallpapercave.com/wp/wp2394254.jpg
+https://c4.wallpaperflare.com/wallpaper/291/819/697/illustration-city-anime-painting-wallpaper-preview.jpg
+
+ */
