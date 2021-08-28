ff9b3e5d84329601938c5121855144130a77198b
Hadley <hadley_dalton@brown.edu>
2021-08-28 04:12:04 -0400

    prog

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,12 @@
-import React from "react";
-
-export default class Header extends React.Component {
-    render(){
-        return (
-            <div>
-                <img class="title-bg" src="https://images.fineartamerica.com/images-medium-large/pastel-sky-john-bartosik.jpg"></img>
-                <div class="circle"></div>
-                <div class="header-style">.hadleyDalton</div>
-                <div class="subtext-style">Designer and coder from {<b>Brown University</b>}</div>
-            </div>
-        )
-    }
+function Header() {
+    return (
+        <div>
+            <img class="title-bg" src="https://images.fineartamerica.com/images-medium-large/pastel-sky-john-bartosik.jpg"></img>
+            <div class="circle"></div>
+            <div class="header-style">.hadleyDalton</div>
+            <div class="subtext-style">Designer and coder from {<b>Brown University</b>}</div>
+        </div>
+    )
 }
+
+export default Header
